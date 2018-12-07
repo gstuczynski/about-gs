d03182b705ac36e09d0e61b21167c910b77c6f5d
Grzegorz Stuczyński <grzegorz.stuczynski@evidenceprime.com>
2018-12-07 18:20:30 +0100

    test

--- src/config.js
+++
@@ -1,7 +0,0 @@
-const hostname = typeof window !== 'undefined' ? window.location.hostname : 'localhost';
-const backendAddress = hostname === 'localhost' ? '//localhost:3000' : `//${hostname}/api`;
-
-export default {
-  backendAddress,
-  allowOrigin: '*',
-};
